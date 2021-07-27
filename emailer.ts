9b2495ad434577c403e6de0aa2f1721f0350e049
W̷̛͒i̶̓͗l̷͗̌ľ̶͘ <62452284+Ugric@users.noreply.github.com>
2021-07-28 00:06:17 +0100

    fix emails

--- emailer.ts
+++ emailer.ts
@@ -65,7 +65,9 @@ async function NotificationEmail(
     subject: `${data.title} | ${data.message}`,
     text: `TypeChat\n\nNEW NOTIFICATION ✨\n\n${data.title}\n${
       data.message
-    }\n\nopen: ${new URL(data.to, "http://localhost:3000/").href}\n© TypeChat`,
+    }\n\nopen: ${
+      new URL(data.to, "http://freshcraft.play.ai:5000/").href
+    }\n© TypeChat`,
     html: `<div style="background-color: #5656ff; color: white"><center><h1 style="color: #ffaa56"><span style="color: #ffff00">Type</span>Chat</h1><div><hr/><div>
     <h3>NEW NOTIFICATION ✨</h3>
     <b>${bleach.sanitize(data.title)}</b>
@@ -82,7 +84,7 @@ async function NotificationEmail(
               <tr>
                   <td style="border-radius: 2px;" bgcolor="#ffff00">
                       <a href=${JSON.stringify(
-                        new URL(data.to, "http://localhost:3000/").href
+                        new URL(data.to, "http://freshcraft.play.ai:5000/").href
                       )} target="_blank" style="padding: 8px 12px; border: 1px solid #ffff00;border-radius: 2px;font-family: Helvetica, Arial, sans-serif;font-size: 14px; color: #ffaa56;text-decoration: none;font-weight:bold;display: inline-block;">
                           Open
                       </a>
