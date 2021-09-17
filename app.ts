1870d0ffe248d6b87d2d4a9627135046ca63a6c4
Ugric <epicugric@gmail.com>
2021-09-17 22:29:55 +0100

    test  and fix force domain

--- app.ts
+++ app.ts
@@ -191,7 +191,7 @@ const messagefunctions = {};
         server: httpsServer
       });
       ws.on('connection', function (ws, req) {
-        console.log(req)
+        console.log(req.url)
         ws.on("/notifications", async (ws, req) => {
           let lastping = 0;
           const connectionID = generate(20);
