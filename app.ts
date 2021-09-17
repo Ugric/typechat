5cd293bd72eb54c89f0bbfd6da39f29180f5c39c
Ugric <epicugric@gmail.com>
2021-09-17 19:02:19 +0100

    secure

--- app.ts
+++ app.ts
@@ -147,7 +147,21 @@ const messagefunctions = {};
   app.use(express.static(path.join(__dirname, "typechat", "build")));
   app.use(cookieParser());
   app.use(require("express-fileupload")());
-  const port = 80;
+  require("greenlock-express")
+    .init({
+        packageRoot: __dirname,
+        configDir: "./greenlock.d",
+ 
+        // contact for security and critical bug notices
+        maintainerEmail: "jon@example.com",
+ 
+        // whether or not to run at cloudscale
+        cluster: false
+    })
+    // Serves on 80 and 443
+    // Get's SSL certificates magically!
+    .serve(app);
+  const port = 5000;
   const getAllOnline = (sockets: {
     [key: string]: { focus: boolean; [key: string]: any };
   }): { focus: boolean; [key: string]: any }[] => {
@@ -1092,8 +1106,9 @@ WHERE accountID == :accountID and toAccountID==:toAccountID
   app.use((_: any, res: any) => {
     res.sendFile(path.join(__dirname, "typechat", "build", "index.html"));
   });
+  `
   app.listen(port, () => {
     console.timeEnd("express boot");
-    console.log(`server started at http://localhost:${port}`);
-  });
+    console.log(\`server started at http://localhost:${port}\`);
+  });`
 })();
