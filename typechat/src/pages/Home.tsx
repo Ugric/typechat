afe09f2e4a49bc025f1b20e9cdba0251b18fe9c6
Ugric <epicugric@gmail.com>
2021-10-10 22:36:04 +0100

    working on homepage

--- typechat/src/pages/Home.tsx
+++ typechat/src/pages/Home.tsx
@@ -1,45 +1,23 @@
 import { Link, Redirect } from "react-router-dom";
 import { useData } from "../hooks/datahook";
-import TypeChat from "../images/logos/TypeChat.svg";
+import appPage from "./images/app page.png";
+import "./css/home.css"
 
 function Home() {
   const { loggedin } = useData();
   if (!loggedin) {
     return (
-      <div>
-        <div>
-          <img
-            src={TypeChat}
-            alt="TypeChat"
-            style={{
-              width: "100%",
-              maxWidth: "250px",
-              display: "block",
-              margin: "1rem auto 0",
-            }}
-          ></img>
-        </div>
-        <p style={{ textAlign: "center" }}>
+      <>
+        <div style={{margin: "1rem"}}>
+        <h1 style={{fontSize:"75px", color: "var(--primary-text-colour)"}}>Hello.</h1>
+        <h1 style={{ color: "var(--secondary-text-colour)"}}>Welcome to TypeChat.</h1>
+        <p>
           Type to your friends. Powered by TypeScript.
         </p>
-        <p style={{ textAlign: "center" }}>
-          <span>
-            <Link to="/login" style={{ color: "var(--secondary-text-colour)" }}>
-              login
-            </Link>
-          </span>{" "}
-          or{" "}
-          <span>
-            <Link
-              to="/signup"
-              style={{ color: "var(--secondary-text-colour)" }}
-            >
-              create an account
-            </Link>
-          </span>
-          !
-        </p>
-      </div>
+        </div>
+        <div style={{width: "100%", backgroundColor: "var(--main-bg-colour)", height: "1px"}}></div>
+        <div style={{textAlign: "center", margin: "1rem", width: "calc(100vw - 2rem)", overflow: "hidden"}}><h3>Add to your homepage to get the best mobile experience!</h3><img className="slideinimage" src={appPage} style={{width: "100%", maxWidth: "350px"}}></img></div>
+      </>
     );
   }
   return <Redirect to="/contacts"></Redirect>;
