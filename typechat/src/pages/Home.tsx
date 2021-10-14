d35d47ce8f545bfcc3697e43ba4a9149310bc04a
Ugric <epicugric@gmail.com>
2021-10-14 20:04:14 +0100

    work on homepage, contacts for latest messages

--- typechat/src/pages/Home.tsx
+++ typechat/src/pages/Home.tsx
@@ -1,23 +1,78 @@
-import { Redirect } from "react-router-dom";
+import { Redirect, useHistory } from "react-router-dom";
 import { useData } from "../hooks/datahook";
 import appPage from "./images/app page.png";
 import "./css/home.css"
+import playSound from "../playsound";
+
+function isRunningStandalone() {
+  return (window.matchMedia('(display-mode: standalone)').matches);
+}
 
 function Home() {
+  const history = useHistory()
+  const standalone = isRunningStandalone();
   const { loggedin } = useData();
   if (!loggedin) {
     return (
-      <>
-        <div style={{margin: "1rem"}}>
-        <h1 style={{fontSize:"75px", color: "var(--primary-text-colour)"}}>Hello.</h1>
-        <h1 style={{ color: "var(--secondary-text-colour)"}}>Welcome to TypeChat.</h1>
-        <p>
+      <div style={{textAlign: "center", padding: "1rem", overflow: "hidden"}}>
+        <h1 className="slideinright" style={{fontSize:"75px", color: "var(--primary-text-colour)", animationDuration: "2s"}}>Hello.</h1>
+        <h1 className="slideinleft" style={{ color: "var(--secondary-text-colour)", animationDuration: "2s", animationDelay: "1s"}}>Welcome to TypeChat.</h1>
+        <p className="slideinright" style={{ animationDuration: "2s", animationDelay: "2s", paddingBottom: "1rem"}}>
           Type to your friends. Powered by TypeScript.
         </p>
+        <div className="logindraw" style={{animationDuration: "2s", animationDelay: standalone?"3s":"5s"}}><div>
+            <button
+            onPointerDown={() => {
+              playSound("/sounds/click2.mp3");
+            }}
+            onPointerUp={() => {
+              playSound("/sounds/click1.mp3");
+            }}
+            onClick={()=>{history.push("/login")}}
+            style={{
+              margin: "0.5rem",
+              padding: "1rem",
+              maxWidth: "250px",
+              width: "100%",
+              border: "none",
+              borderRadius: "50px",
+              background:
+                "linear-gradient(45deg, var(--dark-bg-colour) 0%, var(--light-bg-colour) 100%)",
+              color: "white",
+              fontFamily: '"Source Sans Pro", sans-serif',
+              fontSize: "20px",
+              boxShadow: "rgb(0, 0, 0) 0px 6px 5px 0px",
+            }}
+          >Login</button>
+          <button
+            onPointerDown={() => {
+              playSound("/sounds/click2.mp3");
+            }}
+            onPointerUp={() => {
+              playSound("/sounds/click1.mp3");
+            }}
+            onClick={()=>{history.push("/signup")}}
+            style={{
+              margin: "0.5rem",
+              padding: "1rem",
+              maxWidth: "250px",
+              width: "100%",
+              border: "none",
+              borderRadius: "50px",
+              background:
+                "linear-gradient(45deg, var(--dark-bg-colour) 0%, var(--light-bg-colour) 100%)",
+              color: "white",
+              fontFamily: '"Source Sans Pro", sans-serif',
+              fontSize: "20px",
+              boxShadow: "rgb(0, 0, 0) 0px 6px 5px 0px",
+            }}
+          >Sign Up</button>
+          </div>
         </div>
-        <div style={{width: "100%", backgroundColor: "var(--main-bg-colour)", height: "1px"}}></div>
-        <div style={{textAlign: "center", margin: "1rem", width: "calc(100vw - 2rem)", overflow: "hidden"}}><h3>Add to your homepage to get the best mobile experience!</h3><img className="slideinimage" alt="phone app" src={appPage} style={{width: "100%", maxWidth: "350px"}}></img></div>
-      </>
+        {standalone?<></>:
+        <img className="slideinleft" alt="phone app" src={appPage} style={{width: "100%", maxWidth: "350px", animationDuration: "2s", animationDelay: "3s"}}></img>
+        }
+      </div>
     );
   }
   return <Redirect to="/contacts"></Redirect>;
