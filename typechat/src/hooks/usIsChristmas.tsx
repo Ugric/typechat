51a8939e7b991ab14a761e970c120d92e03c0749
W̷̛͒i̶̓͗l̷͗̌ľ̶͘ <62452284+Ugric@users.noreply.github.com>
2021-12-28 20:04:09 +0000

    add more days to christmas

--- typechat/src/hooks/usIsChristmas.tsx
+++ typechat/src/hooks/usIsChristmas.tsx
@@ -4,36 +4,36 @@ import { useEffect, useState } from "react";
 function useIsChistmas() {
   const date = new Date();
   const [isChristmas, setIsChristmas] = useState(
-    date.getMonth() === 11 && date.getDate() === 25
+    date.getMonth() === 11 && date.getDate() > 25 && date.getDate() < 31
   );
   useEffect(() => {
     const date = new Date();
     let timeout: any;
-      if (date.getMonth() === 11 && date.getDate() === 25) {
+      if (date.getMonth() === 11 && date.getDate()>25 && date.getDate()<31) {
         setIsChristmas(true);
-      timeout = setTimeout(() => {
-        setIsChristmas(false);
-      }, 8.64e7 - (date.getTime() % 8.64e7));
-    } else {
-      const christmas = new Date();
-      christmas.setMonth(11);
-      christmas.setDate(25);
-      christmas.setHours(0);
-      christmas.setMinutes(0);
-          christmas.setSeconds(0);
-          christmas.setMilliseconds(0);
-      let timeToChristmas = christmas.getTime() - date.getTime();
-          if (timeToChristmas < 0) {
-              christmas.setFullYear(christmas.getFullYear() + 1);
-              timeToChristmas = christmas.getTime() - date.getTime();
-            }
-          timeout = setTimeout(() => {
-          setIsChristmas(true);
         timeout = setTimeout(() => {
           setIsChristmas(false);
-        }, 8.64e7);
-      }, timeToChristmas);
-    }
+        }, 8.64e7 - (date.getTime() % 8.64e7));
+      } else {
+        const christmas = new Date();
+        christmas.setMonth(11);
+        christmas.setDate(25);
+        christmas.setHours(0);
+        christmas.setMinutes(0);
+        christmas.setSeconds(0);
+        christmas.setMilliseconds(0);
+        let timeToChristmas = christmas.getTime() - date.getTime();
+        if (timeToChristmas < 0) {
+          christmas.setFullYear(christmas.getFullYear() + 1);
+          timeToChristmas = christmas.getTime() - date.getTime();
+        }
+        timeout = setTimeout(() => {
+          setIsChristmas(true);
+          timeout = setTimeout(() => {
+            setIsChristmas(false);
+          }, 8.64e7*6);
+        }, timeToChristmas);
+      }
     return () => clearTimeout(timeout);
   }, []);
   return isChristmas;
