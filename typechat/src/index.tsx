f95f5600fdda927c5e130ffe672d49d15439e0f3
W̷̛͒i̶̓͗l̷͗̌ľ̶͘ <62452284+Ugric@users.noreply.github.com>
2022-02-13 15:43:21 +0000

    upgrade recaptcha

--- typechat/src/index.tsx
+++ typechat/src/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./index.css";
 import "./pages/css/toggleswitch.css";
 import { BrowserRouter as Router } from "react-router-dom";
